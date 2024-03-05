cbcf0a277513fc9a127803c3229ca2824470bc59
GeetaDesai <geeta.m.desai@gmail.com>
2024-03-05 15:46:22 +0530

    custom KnwoledeAnswerhandler

--- actions/utter_answer.py
+++ actions/utter_answer.py
@@ -1,15 +1,14 @@
 from rasa_sdk import Action
-from rasa_sdk.events import SlotSet
 
 
 class ActionUtterAnswer(Action):
 
-    def name(self) -> Text:
-        return "utter_answer"
+    def name(self):
+        return "action_utter_answer"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    def run(self, dispatcher,
+            tracker,
+            domain):
         answer = tracker.get_slot("answer")  # Get the answer from the slot
         dispatcher.utter_message(text=answer)
 
